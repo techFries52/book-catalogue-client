4482e6e4fb90972cd38666a70068dfb01b68d3ba
jjlrevature <69030071+techFries52@users.noreply.github.com>
2022-10-14 16:51:46 -0500

    csss

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -12,6 +12,7 @@ export class BookListComponent implements OnInit {
   constructor(private bookService:BookService) { }
 
   allBooks:Book[] = [];
+  booksRead:number = 0;
 
   ngOnInit(): void {
     this.bookService.getAllBooksFromServer();
@@ -19,10 +20,20 @@ export class BookListComponent implements OnInit {
 
   ngDoCheck() {
     this.allBooks = this.bookService.getAllBooks();
+    this.booksRead = 0;
+    this.totalBooksRead();    
   }
 
   onClick(book:Book) {
     this.bookService.setCurrentSelection(book);
+  }
+
+  totalBooksRead(): void {
+    this.allBooks.forEach(book => {
+      if(book.haveRead){
+        this.booksRead += 1;
+      }
+    })
   }
 
 }
